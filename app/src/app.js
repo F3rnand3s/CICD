28b7d158d9733290e80fba4fd60a37fd4af9a65a
Hugo <44034816+F3rnand3s@users.noreply.github.com>
2020-06-30 04:39:52 +0100

    Update app.js

--- app/src/app.js
+++ app/src/app.js
@@ -6,4 +6,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
 });
 
+app.get("/hello-world", (req, res) => {
+  res.send('hello world');
+});
+
 module.exports = app;
